b515f4d90c0152fc7df9f7ca8d13de03a71ad350
Montgomery <montgomerywatts@yahoo.com>
2018-07-29 06:43:23 -0400

    Changed schema to better reflect access patterns. Modified addWords, checkWords, and relevant functions to reflect this change. Added documentation.

--- src/main/java/WordCollection.java
+++ src/main/java/WordCollection.java
@@ -2,31 +2,30 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Updates;
 import net.dv8tion.jda.core.entities.User;
 import org.bson.Document;
-import org.bson.codecs.DocumentCodec;
-import org.bson.codecs.EncoderContext;
-import org.bson.json.JsonMode;
-import org.bson.json.JsonWriter;
-import org.bson.json.JsonWriterSettings;
 import org.json.JSONObject;
 
-import java.io.StringWriter;
 import java.util.*;
 
 import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
 
 public class WordCollection {
-    private MongoCollection<Document> serverCollection;
+    private MongoCollection<Document> wordCollection;
     private MongoCollection<Document> userCollection;
 
     public WordCollection(){
         MongoClient client = new MongoClient();
         MongoDatabase db = client.getDatabase("test");
-        serverCollection = db.getCollection("servers");
+        wordCollection = db.getCollection("words");
         userCollection = db.getCollection("users");
     }
+
+    /**
+     * Adds a User to the database
+     * @param user The User to add
+     */
 
     private void addUser(User user){
         Document doc = new Document("_id", user.getId())
@@ -39,92 +38,138 @@ public class WordCollection {
         }
     }
 
+    /**
+     * Adds all passed words to the database. Checks to see if there is a document representing the word first,
+     * and if not it creates one. Then checks a nested document containing information pertaining to what servers
+     * and which people are monitoring that word, and attempts to add the server ID and user ID to the watchlist.
+     * @param serverID String representing the server's ID
+     * @param user The User who would like to be notified when these words are said
+     * @param words String(s) containing the words to add to the database.
+     */
+
+    @SuppressWarnings("unchecked")
     public void addWords(String serverID, User user, String... words){
 
-        //Gets document representing all words on this server to look for
-        Document doc = findByServerId(serverID);
-        if(doc == null) {
-            doc = new Document("_id", serverID)
-                    .append("words", new ArrayList<Document>());
-            serverCollection.insertOne(doc);
-        }
-
-        //Adds user to database if they aren't already in it
-        String userID = user.getId();
-        if(userCollection.find( eq("_id", userID)).first() == null)
+        String userId = user.getId();
+        if( findDocByUserId(userId) == null)
             addUser(user);
 
-        HashMap<String, Document> map = getWordMap(doc);
-        for(String word: words) {
-            //If the word already has someone monitoring it
-            if (map.containsKey(word)) {
+        for (String word: words) {
+            Document doc = findDocByWord(word);
 
-                //If the current user already has the word added to their watchlist, do nothing
-                Document nested = map.get(word);
-                ArrayList<String> users = (ArrayList<String>) nested.get("users");
-                if (users.contains(userID))
-                    return;
-
-                //Update the document if the current user is not on the word's watchlist
-                users.add(userID);
-                serverCollection.updateOne(doc, Updates.pull("words", nested));
-                serverCollection.updateOne(doc, Updates.addToSet("words",
-                        new Document("_id", word).append("users", users)));
-            } else {
-                serverCollection.updateOne(doc, Updates.addToSet("words", new Document("_id", word).append("users", Collections.singletonList(userID))));
+            //Create document for word being added and insert it if it didn't already exist
+            if (doc == null){
+                doc = new Document("_id", word)
+                        .append("servers", new ArrayList<Document>());
+                wordCollection.insertOne(doc);
             }
 
-            //The doc has been modified, so updateOne would not match any documents without this.
-            doc = serverCollection.find(new Document("_id", doc.getString("_id"))).first();
-        }
+            HashMap<String, Document> map = getServerMap(doc);
 
+            //If there is a nested document for the server
+            if (map.containsKey(serverID)){
+                Document nested = map.get(serverID);
+                ArrayList<String> users = (ArrayList<String>) nested.get("users");
+
+                //User is already monitoring this word, attempt to add next word
+                if (users.contains(userId))
+                    continue;
+
+                users.add(userId);
+                wordCollection.updateOne(eq("_id", word), pull("servers", nested));
+                wordCollection.updateOne(eq("_id", word), addToSet("servers",
+                        new Document("_id", serverID).append("users", users)));
+            }
+
+            else {
+                wordCollection.updateOne(doc, addToSet("servers", new Document("_id", serverID).append("users", Collections.singletonList(userId))));
+            }
+        }
     }
 
+    /**
+     * Checks all the words in the passed sentence to see if they contain monitored words. A word can be determined to
+     * be monitored if it has a corresponding document. If a document is found, check its servers array for nested
+     * documents.If the server the message originated from is in the array, add any users to the HashMap so they
+     * may be notified.
+     * @param serverID The ID of the server the message originated from.
+     * @param sentence String representing the message.
+     * @return A HashMap that can be used to alert any users of monitored words, or null.
+     */
+
+    @SuppressWarnings("unchecked")
     public HashMap<String, Entry> checkWords(String serverID, String sentence){
-
-        //replace with searching for the document for the current channel
-        Document doc = findByServerId(serverID);
-        if (doc == null) {
-            return null;
-        }
-
-        HashMap<String, Document> map = getWordMap(doc);
         HashMap<String, Entry> results = new HashMap<>();
+        for (String word: formatString(sentence)){
+            Document doc = findDocByWord(word);
 
-        for (String word: formatString(sentence) ) {
-            if (map.containsKey(word)){
-                Document wordDoc = map.get(word);
-                for (String userID: (ArrayList<String>)wordDoc.get("users")){
+            //Nobody is monitoring that word, check rest of sentence
+            if(doc == null)
+                continue;
 
-                    //Adds entry for a user so they can be messaged
-                    if(!results.containsKey(userID)){
-                        Document userDoc = userCollection.find(eq("_id", userID)).first();
+            HashMap<String, Document> servers = getServerMap(doc);
+
+            //If there is a server that has someone monitoring that word
+            if (servers.containsKey(serverID)){
+                Document serverDoc = servers.get(serverID);
+
+                //Create an entry for each user
+                for (String userId: (ArrayList<String>) serverDoc.get("users") ){
+                    if (!results.containsKey(userId)){
+                        Document userDoc = findDocByUserId(userId);
                         JSONObject user = getJSONFromUserDoc(userDoc);
                         Entry entry = new Entry(user);
                         entry.appendWord(word);
-                        results.put(userID, entry);
-                    } else {
-                        Entry entry = results.get(userID);
+                        results.put(userId, entry);
+                    }
+                    else{
+                        Entry entry = results.get(userId);
                         entry.appendWord(" ");
                         entry.appendWord(word);
-                        results.put(userID, entry);
+                        results.put(userId, entry);
                     }
                 }
             }
+
         }
-
         return results.isEmpty() ? null : results;
-
     }
 
-    private Document findByServerId(String serverID){
-        return serverCollection.find( eq("_id", serverID)).first();
+    /**
+     * Returns a Document from the words collection corresponding to a passed String.
+     * @param word A String representing the word to search for.
+     * @return The corresponding Document or null.
+     */
+
+    private Document findDocByWord(String word){
+        return wordCollection.find( eq("_id", word)).first();
     }
+
+    /**
+     * Returns a Document from the users collection corresponding to a passed String.
+     * @param userId A String representing the ID of the user to search for.
+     * @return The corresponding Document or null.
+     */
+
+    private Document findDocByUserId(String userId){
+        return userCollection.find( eq("_id", userId)).first();
+    }
+
+    /**
+     * Removes punctuation from a given string and splits it so it may be processed by checkWords
+     * @param string The String to format
+     * @return A String[] that can be iterated through by checkWords
+     */
 
     private static String[] formatString(String string){
         return string.replaceAll("[^a-zA-Z0-9' ]", "").toLowerCase().split(" ");
     }
 
+    /**
+     * Creates a JSONObject from a Document in the users collection. Used by JDA to recreate a User Object.
+     * @param doc The Document containing the user's information.
+     * @return A JSONObject that can be used to recreate the User.
+     */
     private JSONObject getJSONFromUserDoc(Document doc){
         return new JSONObject()
                 .put("id", doc.getString("_id"))
@@ -132,10 +177,38 @@ public class WordCollection {
                 .put("discriminator", doc.getString("disc"));
     }
 
+    /**
+     * Returns a List of the Documents in a word Document's server array
+     * @param doc A Document from the words collection to retrieve the nested Documents of.
+     * @return A List of the nested server Documents
+     */
+
+    @SuppressWarnings("unchecked")
+    private static List<Document> getServerDocs(Document doc){
+        return (List<Document>) doc.get("servers");
+    }
+
+    /**
+     * Uses getServerDocs to map the ID of the server Documents to the Documents themselves. Useful when determining
+     * if a word Document already contains a nested Document for a specified server.
+     * @param doc A Document from the words collection to retrieve and map the nested Documents of.
+     * @return A HashMap mapping the Documents' IDs to the Documents themselves.
+     */
+
+    private static HashMap<String, Document> getServerMap(Document doc){
+        HashMap<String, Document> map = new HashMap<>();
+        for(Document d: getServerDocs(doc)) {
+            map.put(d.getString("_id"), d);
+        }
+
+        return map;
+    }
+
+    //THIS NEEDS TO BE CHANGED TO REFLECT NEW SCHEMA
     public String getMonitoredWords(String serverID, String userID){
         StringBuilder builder = new StringBuilder();
 
-        Document main = serverCollection.find( eq("_id", serverID)).first();
+        Document main = wordCollection.find( eq("_id", serverID)).first();
         if (main == null)
             return null;
 
@@ -150,26 +223,9 @@ public class WordCollection {
         return builder.toString();
     }
 
+    //TO BE REMOVED/CHANGED TO REFLECT NEW SCHEMA
     @SuppressWarnings("unchecked")
     private static List<Document> getWordDocs(Document doc){
         return (List<Document>) doc.get("words");
     }
-
-    /**
-     * Returns a HashMap that maps the word representing the document's _id to the document itself
-     * @param doc The document that contains the words array
-     * @return A HashMap mapping the word to its corresponding document. e.g. frick would map to a Document
-     * like { "_id" : "frick", "users" : [ "12345" ] }
-     */
-
-    private static HashMap<String, Document> getWordMap(Document doc){
-        HashMap<String, Document> map = new HashMap<>();
-        for(Document d: getWordDocs(doc)) {
-            map.put(d.getString("_id"), d);
-        }
-
-        return map;
-    }
-
-
 }
